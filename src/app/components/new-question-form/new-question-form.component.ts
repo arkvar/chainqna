c875ef084bbb221496753db65064896506ab1689
seriousownsya <seriousownsya@gmail.com>
2017-10-02 20:36:11 +0200

    - allows posting of new questions with manual input of username and posting key, fixed tags ('chainqna', 'chainqna-test') and fixed beneficiaries. perm-links generated based on timestamps

--- src/app/components/new-question-form/new-question-form.component.ts
+++ src/app/components/new-question-form/new-question-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '@app/core/user/user.service';
 import {Question} from '@app/question';
+import {User} from '@app/user';
+import {QuestionsService} from '@app/core/questions/questions.service';
 
 @Component({
   selector: 'app-new-question-form',
@@ -10,15 +12,21 @@ import {Question} from '@app/question';
 export class NewQuestionFormComponent implements OnInit {
   submitted = false;
   model = new Question();
+  user = new User();
+  qs: any;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, questionsService: QuestionsService) {
     this.model.author = userService.getUser().name;
+    this.qs = questionsService;
   }
 
   ngOnInit() {
   }
 
-  onSubmit() { this.submitted = true; }
+  onSubmit() {
+    this.qs.newQuestion(this.model, this.user);
+    this.submitted = true;
+  }
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,7 @@
 export class User {
   name: String;
   postKey: String;
-  constructor(name: String, postKey: String) {
+  constructor(name: String = '', postKey: String = '') {
     this.name = name;
     this.postKey = postKey;
   }
